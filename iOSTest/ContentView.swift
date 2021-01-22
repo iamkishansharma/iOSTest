0dc99fa23a79f5d04dc92a6c8eea0996aaedfc23
Kishan Kr Sharma <skishan781@gmail.com>
2021-01-22 14:16:01 +0545

    login page & Alert learned

--- iOSTest/ContentView.swift
+++ iOSTest/ContentView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myStateVariable:String = ""
+    @State var myEmailState:String = ""
+    @State var myPasswordState:String = ""
+    @State var myNameState:String = ""
     
+    @State private var showingAlert = false
     var body: some View {
         VStack{
             
@@ -21,18 +24,33 @@ struct ContentView: View {
                 .padding(.all,100.0)
                 .multilineTextAlignment(.center);
             
-            TextField("Your Name", text: $myStateVariable)
+            TextField("Full Name", text: $myNameState)
                 .padding(.all,10.0)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
                             .shadow(color: .black, radius: 10.0));
             
-            Text("Welcome ! \(myStateVariable)")
+            TextField("Email", text: $myEmailState)
+                .padding(.all,10.0)
+                .textContentType(.emailAddress)
+                .overlay(RoundedRectangle(cornerRadius: 10.0)
+                            .stroke(lineWidth: 2.0)
+                            .shadow(color: .black, radius: 10.0));
+            TextField("Password", text: $myPasswordState)
+                .padding(.all,10.0)
+                .textContentType(.password)
+                .overlay(RoundedRectangle(cornerRadius: 10.0)
+                            .stroke(lineWidth: 2.0)
+                            .shadow(color: .black, radius: 10.0));
+            
+            Text("Welcome ! \(myNameState)")
                 .italic()
                 .font(.body);
             
             Button(action:{
-                print("Button clicked \(myStateVariable)")
+                print("Email: \(myEmailState)")
+                print("Password: \(myPasswordState)")
+                self.showingAlert = true
             }){
                 Text("Click Me")
                     .padding(10.0)
@@ -40,7 +58,9 @@ struct ContentView: View {
                                 .stroke(lineWidth: 2.0)
                                 .shadow(color: .blue, radius: 10.0))
             }
-            
+            .alert(isPresented: $showingAlert){
+                Alert(title:Text("Important notice \(myNameState)"),message: Text("Are you sure you want to desable this alert?"),dismissButton: .default(Text("Alright Cool!")))
+            }
         }
     }
     
