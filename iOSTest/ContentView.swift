1f53ec0519ae12e2ba63be88b3ff5566d670d7c7
Kishan Kr Sharma <skishan781@gmail.com>
2021-01-27 14:05:45 +0545

    Image & Shapes learned

--- iOSTest/ContentView.swift
+++ iOSTest/ContentView.swift
@@ -15,42 +15,66 @@ struct ContentView: View {
     @State private var showingAlert = false
     var body: some View {
         VStack{
+            Image(systemName: "person")
+                .font(.system(size: 100))
+                .foregroundColor(.black)
+                .shadow(color: .gray, radius: 10, x: 5, y: 10);
+            
+            Image(systemName: "cloud.sun.rain")
+                .resizable()
+                .foregroundColor(.black)
+                .background(Color.blue)
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .padding(.all, 20)
+                .aspectRatio(contentMode: .fill)
+                .shadow(color: .gray, radius: 10, x: 5, y: 5);
+            
             
             Text("HeyCode Inc.")
                 .bold()
                 .font(.title)
-                .background(Color.blue, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .background(Color.red, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .clipShape(Capsule())
+                .shadow(color: .gray, radius: 10, x: 5, y: 5)
                 .foregroundColor(.white)
-                .padding(.all,100.0)
                 .multilineTextAlignment(.center);
             
             TextField("Full Name", text: $myNameState)
                 .padding(.all,10.0)
+                .foregroundColor(.white)
+                .accentColor(.white)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
                             .shadow(color: .black, radius: 10.0));
             
             TextField("Email", text: $myEmailState)
                 .padding(.all,10.0)
+                .foregroundColor(.white)
                 .textContentType(.emailAddress)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
                             .shadow(color: .black, radius: 10.0));
             TextField("Password", text: $myPasswordState)
                 .padding(.all,10.0)
-                .textContentType(.password)
+                .foregroundColor(.white)
+                .textContentType(.newPassword)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
                             .shadow(color: .black, radius: 10.0));
             
             Text("Welcome ! \(myNameState)")
                 .italic()
+                .foregroundColor(.white)
                 .font(.body);
             
             Button(action:{
                 print("Email: \(myEmailState)")
                 print("Password: \(myPasswordState)")
                 self.showingAlert = true
+                if(!showingAlert){
+                    
+                }
             }){
                 Text("Click Me")
                     .padding(10.0)
@@ -63,7 +87,6 @@ struct ContentView: View {
             }
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
