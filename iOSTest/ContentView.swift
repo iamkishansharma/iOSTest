68e47a5a0b2aaaa2633e5fb84fe19c6eb1031343
Kishan Kr Sharma <skishan781@gmail.com>
2021-01-28 16:40:55 +0545

    More ui improvement

--- iOSTest/ContentView.swift
+++ iOSTest/ContentView.swift
@@ -14,52 +14,85 @@ struct ContentView: View {
     
     @State private var showingAlert = false
     var body: some View {
-        VStack{
-            Image(systemName: "person")
-                .font(.system(size: 100))
-                .foregroundColor(.black)
-                .shadow(color: .gray, radius: 10, x: 5, y: 10);
+        VStack(alignment: .center, spacing: 10){
+//            Image(systemName: "person")
+//                .font(.system(size: 100))
+//                .foregroundColor(.black)
+//                .shadow(color: .gray, radius: 10, x: 5, y: 10);
             
-            Image(systemName: "cloud.sun.rain")
+//            Image(systemName: "cloud.sun.rain")
+//                .resizable()
+//                .foregroundColor(.black)
+//                .background(Color.blue)
+//                .frame(width: 200, height: 200)
+//                .clipShape(Circle())
+//                .padding(.bottom, 20)
+//                .aspectRatio(contentMode: .fill)
+//                .shadow(color: .gray, radius: 10, x: 5, y: 5);
+            
+            Image("heyk")
                 .resizable()
-                .foregroundColor(.black)
-                .background(Color.blue)
-                .frame(width: 200, height: 200)
+                .frame(width:250, height:250, alignment: .center)
                 .clipShape(Circle())
-                .padding(.all, 20)
-                .aspectRatio(contentMode: .fill)
+                .aspectRatio(contentMode: .fit)
                 .shadow(color: .gray, radius: 10, x: 5, y: 5);
             
+            HStack(spacing: 10){
+                Text("Kishan")
+                    .bold()
+                    .font(.title)
+                    .padding(.all,10.0)
+                    .background(Color.red, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(20)
+                    .shadow(color: .gray, radius: 10, x: 5, y: 5)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.leading);
+                
+                Text("Kumar")
+                    .bold()
+                    .font(.title)
+                    .padding(.all,10.0)
+                    .background(Color.red, alignment: .center)
+                    .cornerRadius(20)
+                    .shadow(color: .gray, radius: 10, x: 5, y: 5)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center);
+                
+                Text("Sharma")
+                    .bold()
+                    .font(.title)
+                    .padding(.all,10.0)
+                    .background(Color.red, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .shadow(color: .gray, radius: 10, x: 5, y: 5)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .multilineTextAlignment(.center);
+                
+            }.background(Color.black)
+            .cornerRadius(20)
+            .padding(.all,10)
+            .shadow(color: .gray, radius: 10, x: 5, y: 5);
             
-            Text("HeyCode Inc.")
-                .bold()
-                .font(.title)
-                .background(Color.red, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .clipShape(Capsule())
-                .shadow(color: .gray, radius: 10, x: 5, y: 5)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center);
             
-            Spacer(minLength: 10.0)
             
             TextField("Full Name", text: $myNameState)
                 .padding(.all,10.0)
-                .foregroundColor(.white)
-                .accentColor(.white)
+                .foregroundColor(.black)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
                             .shadow(color: .black, radius: 10.0));
-            
+    
             TextField("Email", text: $myEmailState)
                 .padding(.all,10.0)
-                .foregroundColor(.white)
+                .foregroundColor(.black)
                 .textContentType(.emailAddress)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
                             .shadow(color: .black, radius: 10.0));
+            
             TextField("Password", text: $myPasswordState)
                 .padding(.all,10.0)
-                .foregroundColor(.white)
+                .foregroundColor(.black)
                 .textContentType(.newPassword)
                 .overlay(RoundedRectangle(cornerRadius: 10.0)
                             .stroke(lineWidth: 2.0)
@@ -67,7 +100,8 @@ struct ContentView: View {
             
             Text("Welcome ! \(myNameState)")
                 .italic()
-                .foregroundColor(.white)
+                .foregroundColor(.black)
+                .padding(.bottom, 10.0)
                 .font(.body);
             
             Button(action:{
@@ -79,14 +113,16 @@ struct ContentView: View {
                 }
             }){
                 Text("Click Me")
-                    .padding(10.0)
+                    .padding(.all,10.0)
                     .overlay(RoundedRectangle(cornerRadius: 10.0)
                                 .stroke(lineWidth: 2.0)
                                 .shadow(color: .blue, radius: 10.0))
             }
             .alert(isPresented: $showingAlert){
-                Alert(title:Text("Important notice \(myNameState)"),message: Text("Are you sure you want to desable this alert?"),dismissButton: .default(Text("Alright Cool!")))
+                Alert(title:Text("Important notice \(myNameState)"),message: Text("Are you sure you want to desable this alert?"), primaryButton: .default(Text("Ok")), secondaryButton: .default(Text("Cancle")))
             }
+            
+            Spacer(minLength: 10.0)
         }
     }
 }
